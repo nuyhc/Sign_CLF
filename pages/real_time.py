aab48ddd56d55a96e5d607a8fcee896810c0c8e8
sorijoo <sorij95@gmail.com>
2022-07-22 11:53:39 +0900

    test

--- pages/real_time.py
+++ pages/real_time.py
@@ -10,6 +10,12 @@ import seaborn as sns
 from PIL import Image
 import pillow_heif
 import cv2
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+import time
 
 # import splitfolders
 
@@ -27,9 +33,76 @@ import av
 
 device = 'cpu'
 if not hasattr(st, 'classifier'):
-    st.model = model = keras.models.load_model('model/eng_sign_lang_cnn_epochs50_27.h5')
+    model = keras.models.load_model("../model/keras_model.h5")
     # st.model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt', _verbose=False)
+
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("../model/keras_model.h5", "../model/labels.txt")
+ 
+offset = 20
+imgSize = 300
+ 
+
+labels = [chr(x).upper() for x in range(97, 123)]
+labels.remove("J")
+labels.remove("Z")
+ 
+while True:
+    try:
+        ret, img = cap.read()
+        imgOutput = img.copy()
+        hands, img = detector.findHands(img)
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand['bbox']
     
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+    
+            imgCropShape = imgCrop.shape
+    
+            aspectRatio = h / w
+    
+            if aspectRatio > 1:
+                k = imgSize / h
+                wCal = math.ceil(k * w)
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                imgResizeShape = imgResize.shape
+                wGap = math.ceil((imgSize - wCal) / 2)
+                imgWhite[:, wGap:wCal + wGap] = imgResize
+                prediction, index = classifier.getPrediction(imgWhite, draw=False)
+                # print(prediction, index)
+    
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize
+                prediction, index = classifier.getPrediction(imgWhite, draw=False)
+    
+            cv2.rectangle(imgOutput, (x - offset, y - offset-50),
+                        (x - offset+90, y - offset-50+50), (255, 0, 139), cv2.FILLED)
+            cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+            cv2.rectangle(imgOutput, (x-offset, y-offset),
+                        (x + w+offset, y + h+offset), (255, 0, 139), 4)
+    
+    
+            cv2.imshow("ImageCrop", imgCrop)
+            cv2.imshow("ImageWhite", imgWhite)
+    except:
+        print("카메라가 경계선 밖으로 나갔습니다.")
+        break
+ 
+    cv2.imshow("Image", imgOutput)
+    cv2.waitKey(1)
+    
+cap.release()
+cv2.destroyAllWindows()
+
 
 
 RTC_CONFIGURATION = RTCConfiguration(
@@ -46,7 +119,7 @@ class VideoProcessor:
 
         # model processing
         im_pil = Image.fromarray(flipped)
-        results = st.model(im_pil, size=112)
+        results = model(im_pil, size=112)
         bbox_img = np.array(results.render()[0])
 
         return av.VideoFrame.from_ndarray(bbox_img, format="bgr24")
