66ee34282182c4d5f5192299b4106ab1588ff9eb
sorijoo <sorij95@gmail.com>
2022-07-22 15:40:12 +0900

    test

--- pages/kornumber.py
+++ pages/kornumber.py
@@ -105,8 +105,8 @@ def img_resize_to_gray(filename):
 
 
 if filename is not None:
-    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    img = cv2.cvtColor(filename, cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img, (300, 300))    
     plt.figure(figsize=(4, 4))
     plt.imshow(img)
